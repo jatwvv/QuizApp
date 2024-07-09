b641cb668b2e2a61637fc60d460e46c99456d626
jatwvv <146978372+jatwvv@users.noreply.github.com>
2024-07-09 20:17:05 +0300

    Update end.js

--- end.js
+++ end.js
@@ -13,7 +13,7 @@ username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = (e) => {
+highScores = (e) => {
   e.preventDefault();
 
   const score = {
